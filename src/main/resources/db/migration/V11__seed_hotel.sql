a23d9db6c7513e5e0a046a4a6e339aae2891defc
Sasa <sasastanisic4@gmail.com>
2023-06-27 09:45:19 +0200

    Seeded data for hotel and travel domain

---
+++ src/main/resources/db/migration/V11__seed_hotel.sql
@@ -0,0 +1,3 @@
+INSERT INTO `hotel` (name, stars, hotel_type, available_rooms, price_per_night, picture_url, destination_id)
+VALUES ('Astarte Suites', 5, 'HOTEL', 30, 250, 'astarte-suites.png', 1),
+       ('Le Sirenuse', 5, 'RESORT', 25, 3000, 'sirenuse-positano.jpeg', 6);
---
+++ src/main/resources/db/migration/V12__seed_travel.sql
@@ -0,0 +1,3 @@
+INSERT INTO `travel` (name, departure_day, travel_duration, transport_type, number_of_nights, destination_id)
+VALUES ('Golden Hour', '2023-06-28', 'WEEK', 'PLANE', 6, 1),
+       ('Italian Dream', '2023-07-04', 'WEEKEND', 'BUS', 2, 6);
